61bca6b62cea92364d123d724bff23f76e111cc0
kjy <visionjyk21@naver.com>
2021-12-09 13:48:11 +0900

    Initial Commit

---
+++ realmTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  realmTestApp
+//
+//  Created by 김주영 on 2021/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
